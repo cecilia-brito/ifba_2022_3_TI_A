e7838368685cdfa3d15dff414229fa206e8315a7
Cecília Brito <ceciliabritosantos@gmail.com>
2022-04-19 09:34:35 -0300

    Add files via upload

---
+++ javaapplication8/Conta.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author aluno
+ */
+
+import javax.swing.JOptionPane;
+
+public class Conta {
+        int bankCode;
+        String bankName;
+        long userCpf;
+        int numCount;
+        int numAgencia;
+        String userName;
+        double saldo;
+        String password;
+        
+        public void depositar(double money){
+           
+            String userPass = JOptionPane.showInputDialog("Digite sua senha para depositar"); 
+            if(money > 0 && this.password.equals(userPass)){
+                this.saldo += money;
+            } else{
+                System.out.println("");
+            }
+        }
+        
+        public void sacar(double money){
+            String userPass = JOptionPane.showInputDialog("Digite sua senha para sacar"); 
+            if(money > 0 && this.saldo > 0 && this.password.equals(userPass)){
+                this.saldo -= money;
+            }
+        }
+        
+        public void transferir(Conta contaTransf, double value){
+            String userPass = JOptionPane.showInputDialog("Digite sua senha para transferir"); 
+            if(value > 0 && this.saldo > 0 &&  this.password.equals(userPass)){
+                this.saldo -= value;
+                contaTransf.saldo += value;
+            }
+             System.out.println("Transferência realizada com suscesso");
+             System.out.println(this.bankCode);
+             System.out.println(this.bankName);
+             System.out.println(this.userCpf);
+             System.out.println(this.numCount);
+             System.out.println(this.numAgencia);
+             System.out.println(this.userName);
+             System.out.println("Seu saldo atual é" + this.saldo);
+             System.out.println("Valor "+ value +" transferido para conta: " + contaTransf.numCount);
+             System.out.println("Cpf: " + contaTransf.userCpf);
+             System.out.println("Banco: " + contaTransf.bankName + " " + contaTransf.bankCode);
+        }
+        public void mostrarStatus(){
+             System.out.println("bank code" + this.bankCode);
+             System.out.println("bank name" + this.bankName);
+             System.out.println("cpf" + this.userCpf);
+             System.out.println("numero da conta" + this.numCount);
+             System.out.println("numero da agendia" + this.numAgencia);
+             System.out.println("username" + this.userName);
+             System.out.println("saldo" + this.saldo);
+        }
+
+}
---
+++ javaapplication8/JavaApplication8.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author aluno
+ */
+import javax.swing.*;
+import javax.swing.JOptionPane;
+
+public class JavaApplication8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Conta novaConta = new Conta();
+        
+        JOptionPane.showMessageDialog(null, "Cadastro de nova conta");
+        
+        novaConta.bankCode = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do banco")) ;
+        novaConta.bankName = JOptionPane.showInputDialog("Digite o nome do banco");
+        novaConta.numAgencia = Integer.parseInt(JOptionPane.showInputDialog("Digite o código da agência"));
+        novaConta.numCount = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da conta"));
+        novaConta.userCpf = Integer.parseInt(JOptionPane.showInputDialog("Digite o cpf do usuário"));
+        novaConta.userName = JOptionPane.showInputDialog("Digite o seu nome");
+        novaConta.saldo = Integer.parseInt(JOptionPane.showInputDialog("Digite seu saldo inicial"));
+        novaConta.password = JOptionPane.showInputDialog("Digite a sua senha");
+        
+        Conta c2 = new Conta();
+        
+        JOptionPane.showMessageDialog(null, "Cadastro de nova conta");
+        
+        c2.bankCode = Integer.parseInt(JOptionPane.showInputDialog("Digite o código do banco")) ;
+        c2.bankName = JOptionPane.showInputDialog("Digite o nome do banco");
+        c2.numAgencia = Integer.parseInt(JOptionPane.showInputDialog("Digite o código da agência"));
+        c2.numCount = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da conta"));
+        c2.userCpf = Integer.parseInt(JOptionPane.showInputDialog("Digite o cpf do usuário"));
+        c2.userName = JOptionPane.showInputDialog("Digite o seu nome");
+        c2.saldo = Integer.parseInt(JOptionPane.showInputDialog("Digite seu saldo inicial"));
+        c2.password = JOptionPane.showInputDialog("Digite a sua senha");
+        
+        double deposito = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do deposito"));
+        novaConta.depositar(deposito);
+        novaConta.mostrarStatus();
+        
+        double saque = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor do saque"));
+        novaConta.sacar(saque);
+         double transf = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor da transferência"));
+        novaConta.transferir(c2,transf);
+        
+        
+   
+    }
+    
+}
