e4806a1dabda7204f88c1c83d61c4b5d8b930657
Cecília Brito <ceciliabritosantos@gmail.com>
2022-05-19 11:08:53 -0300

    Update Principal.java

--- lista_1_lpII_ciclo_1/Escola/src/Principal.java
+++ lista_1_lpII_ciclo_1/Escola/src/Principal.java
@@ -7,16 +7,35 @@
  *
  * @author cecilia brito 3TI-A | IFBA
  */
+import java.util.Scanner;
 public class Principal {
-     public static void main(String[] args) {
-       Aluno aluna2022 = new Aluno();
-       aluna2022.setMatricula("20221233832");
-       aluna2022.setNomeAluno("Sophia Alameda");
-       aluna2022.setNota_prova1(7.4);
-       aluna2022.setNota_prova2(4.5);
-       aluna2022.setNota_trabalho(6.5);
-       
-       aluna2022.calcularMedia();
-       aluna2022.estaAprovado();
-     }
+    public static void main(String[] args) {
+        
+        Scanner s1 = new Scanner(System.in);
+        Aluno aluna2022 = new Aluno();
+        String keyboard;     
+        
+        System.out.println("Digite a matrícula do aluno: ");
+        keyboard = s1.next();
+        aluna2022.setMatricula(keyboard);
+        
+        System.out.println("Digite o nome do aluno: ");
+        keyboard = s1.next();
+        aluna2022.setNomeAluno(keyboard);
+        
+        System.out.println("Digite a nota da prova 1 do aluno: ");
+        keyboard = s1.next();
+        aluna2022.setNota_prova1(Double.parseDouble(keyboard));
+        
+        System.out.println("Digite a nota da prova 2 do aluno: ");
+        keyboard = s1.next();
+        aluna2022.setNota_prova2(Double.parseDouble(keyboard));
+        
+        System.out.println("Digite a nota do trabalho: ");
+        keyboard = s1.next();
+        aluna2022.setNota_trabalho(Double.parseDouble(keyboard));
+                
+        aluna2022.calcularMedia();
+        aluna2022.estaAprovado();
+    }
 }
